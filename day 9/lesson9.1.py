cfa315f793801fb775fb363f0028f92807590f6b
saba123saba <sabasaba262009@gmail.com>
2024-03-11 14:06:35 +0400

    second commit

---
+++ day 9/lesson9.1.py
@@ -0,0 +1,7 @@
+#1) მომხმარებელს შეეკითხეთ სურს თუ არა ბულიონი, მომხმარებელმა უნდა შეიყვანოს true and false და უნდა დამპრინტოთ.
+
+bulioni = bool(input(" გიყვარს ბულიონი?  :"))
+
+print("bulion love is " + str(bulioni) + " for me")
+print(type(bulioni))
+
---
+++ day 9/lesson9.2.py
@@ -0,0 +1,8 @@
+#2)  მომხმარებელს შემოატანინეთ ორი რიცხვი, მისი ასაკი და მშობლის ასაკი, შემდეგ კი შეადარეთ ისინი ერთმანეთს.
+
+your_age = int(input(" type your age:  "))
+parent_age = int(input(" type you parents age:  "))
+
+comperation = your_age > parent_age
+
+print(comperation)
