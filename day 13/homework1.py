fbd225b06b15f1e3f019ef5e90e99e70bcc61e5a
saba123saba <sabasaba262009@gmail.com>
2024-03-18 23:28:02 +0400

    second commit

--- day 13/homework1.py
+++ day 13/homework1.py
@@ -5,8 +5,9 @@ score4 = int(input("Enter score4: "))
 
 sum = (score1 + score2 + score3 + score4) / 4 
 
-print(sum)
-# if sum > 8 and sum < 4:
-#     print(" ყოჩაღ, კარგი შედეგია! ")
-# else:
-#     print(" ცუდი შედეგია, გამოასწორე! ")
+# print(sum)
+
+if sum > 8 and sum < 4:
+    print(" ყოჩაღ, კარგი შედეგია! ")
+else:
+    print(" ცუდი შედეგია, გამოასწორე! ")
---
+++ day 13/homework3.py
@@ -0,0 +1,4 @@
+question = input("შენი სახელი და გვარი?!:  ")
+
+for i in range(1, 100):
+    print(str(i)+ ")saba kublashvili")
